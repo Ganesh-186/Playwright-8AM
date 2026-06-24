6d136152d47c82db3289753dc3c3f044632b06f5
Ganesh-186 <ganeshravichandran18@gmail.com>
2026-06-24 08:56:08 +0530

    all files removed

--- tests/drop.spec.js
+++
@@ -1,8 +0,0 @@
-import {test} from '@playwright/test'
-
-test('Drop_Down',async({page})=>{
-    await page.goto('https://www.amazon.in/');
-    await page.selectOption('#searchDropdownBox','Books');
-    await page.waitForTimeout(4000);
-
-})
