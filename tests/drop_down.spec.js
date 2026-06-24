92ad24feb860576873a643a6b586df7076a8ba9b
Ganesh-186 <ganeshravichandran18@gmail.com>
2026-06-24 08:52:34 +0530

    Undo

--- tests/drop_down.spec.js
+++
@@ -1,8 +0,0 @@
-import {test} from '@playwright/test'
-
-test('Multi Select',async({page})=>{
-    await page.goto('https://www.testautomationcentral.com/demo/dropdown.html');
-    await page.click('[data-target="multi-select-dropdown"]');
-    await page.selectOption('[class="form-multiselect block w-full mt-1"]',['Option 3','Option 1','Option 4']);
-    await page.waitForTimeout(4000);
-})
